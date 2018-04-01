02132352d360938c80be935fcdf338853efafd03
Oleksii Kachura <alex.kachura@gmail.com>
2018-04-01 19:25:03 +0200

    remove logs

--- src/store/Store.js
+++ src/store/Store.js
@@ -5,8 +5,6 @@ const store = {}
 window.store = store
 
 function reset() {
-  console.log('reset')
-
   Object.assign(store, {
     entries: [],
     mouseHistory: [],
@@ -15,20 +13,14 @@ function reset() {
 }
 
 function getValue(fieldPath) {
-  console.log('get', fieldPath)
-
   return get(store, fieldPath)
 }
 
 function setValue(fieldPath, value) {
-  console.log('set', fieldPath, value)
-
   return set(store, fieldPath, value)
 }
 
 function getStore() {
-  console.log('getStore')
-
   return store
 }
 
